caab33934ad1c8c76ca462b584815f2912ff09de
Arestoteless <baresmanggala27@gmail.com>
2021-12-20 01:48:06 +0700

    Cardslist udh dimasukkin tinggal tambah illust

--- dashboard.php
+++ dashboard.php
@@ -9,24 +9,22 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style2.css"/>
+    <link rel="stylesheet" href="dashboard.css"/>
     <title>Dashboard | Catatanmu.id</title>
 </head>
 
 <body>
-    <header class="navbar">
         <nav>
             <h1 class="catatanmu">Catatanmu.id</h1>
             <ul class="nav_links">
-                <li>
-                    <a class="db_ref" href="dashboard.php">Dashboard</a>
+                <li><a class="db_ref" href="dashboard.php">Dashboard</a></li>
+                <li>    
                     <a class="userlogo" href="profile.php"> 
-                        <img class="profilelogo" src="img/Avatar.svg" width="40em" href="profile.php">
+                        <img class="profilelogo" src="img/Avatar.svg" width="50em" href="profile.php">
                     </a>
-                <li>
+                </li>
             </ul>
         </nav>
-    </header>
 
     <h1 class="greeting">Welcome! <?php echo $_SESSION["user"]["username"] ?></h1>
 
@@ -35,21 +33,41 @@
             <div class="cardslist">
                 <h2>To-do List</h2>
                 <img src="#" alt="TDL">
-                <a class="More" href="tdl.php">More</a>
+                <a class="more" href="tdl.php">More</a>
             </div>
 
             <div class="cardslist">
                 <h2>Wishlist</h2>
                 <img src="#" alt="WL">
-                <a class="More" href="wl.php">More</a>
+                <a class="more" href="wl.php">More</a>
             </div>
 
             <div class="cardslist">
                 <h2>Refreshing List</h2>
                 <img src="#" alt="RL">
-                <a class="More" href="rl.php">More</a>
+                <a class="more" href="rl.php">More</a>
             </div>
         </div>
     </div>
+    
+    <!--<div class="cardslist">
+        <h2>To-do List</h2>
+        <img src="#" alt="TDL">
+        <a class="More" href="tdl.php">More</a>
+    </div>
+
+    <div class="cardslist">
+        <h2>Wishlist</h2>
+        <img src="#" alt="WL">
+        <a class="More" href="wl.php">More</a>
+    </div>
+
+    <div class="cardslist">
+        <h2>Refreshing List</h2>
+        <img src="#" alt="RL">
+        <a class="More" href="rl.php">More</a>
+    </div>
+-->
+
 </body>
 </html>
