253af0c1a05b56caa516f26f822e4fb8f1b4d99f
faizbyp <faizbyp@gmail.com>
2024-04-27 10:07:21 +0700

    change config to prod

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
     $conf = parse_ini_file(__DIR__ . "/conf.ini", true);
 
     // variabel database
-    $servername = $conf["db_dev"]["hostname"];
-    $username = $conf["db_dev"]["username"];
-    $password = $conf["db_dev"]["password"];
-    $database = $conf["db_dev"]["database"];
+    $servername = $conf["db_prod"]["hostname"];
+    $username = $conf["db_prod"]["username"];
+    $password = $conf["db_prod"]["password"];
+    $database = $conf["db_prod"]["database"];
 
     // koneksi ke database menggunakan mysqli
     $conn = mysqli_connect($servername, $username, $password, $database);
