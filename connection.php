8f99176eda1f559b5bcd7dc1271973ffc2608ab4
faizbyp <faizbyp@gmail.com>
2024-06-05 20:50:54 +0700

    tidy

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
     $conf = parse_ini_file(__DIR__ . "/conf.ini", true);
 
     // variabel database
-    $servername = $conf["db_prod"]["hostname"];
-    $username = $conf["db_prod"]["username"];
-    $password = $conf["db_prod"]["password"];
-    $database = $conf["db_prod"]["database"];
+    $servername = "localhost";
+    $username = "faizbypm_cttn";
+    $password = "!AzrielBaresFaizHanif123";
+    $database = "faizbypm_cttnmu";
 
     // koneksi ke database menggunakan mysqli
     $conn = mysqli_connect($servername, $username, $password, $database);
